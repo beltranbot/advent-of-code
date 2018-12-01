55b4d2f6baaedffea5f3f2f30977c19a4f8a9e10
hx <beltranbot@gmail.com>
2018-12-01 00:23:59 -0500

    refactor and using a hash instead of an array

--- 2018/001/solve.js
+++ 2018/001/solve.js
@@ -1,21 +1,19 @@
 function solve1(input) {
   input = input.split('\n').map(x => +x)
-  return input.reduce((a,c) => a +c , 0)
-
-
+  return input.reduce((a, c) => a + c, 0)
 }
 
-function solve2 (input) {
+function solve2(input) {
   input = input.split('\n').map(x => +x)
-  let frecuencies = []
+  let frecuencies = {}
   let ans = 0
   while (true) {
     for (let i = 0; i < input.length; i++) {
       ans += input[i]
-      if (frecuencies.includes(ans)) {
-        return ans
+      if (!frecuencies[ans]) {
+        frecuencies[ans] = true
       } else {
-        frecuencies.push(ans)
+        return ans
       }
     }
   }
